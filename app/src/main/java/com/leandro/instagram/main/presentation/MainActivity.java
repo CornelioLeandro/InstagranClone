ee460a40e9147d8d27bf8831566a3664e33d1774
InstagramClone <cornelio.leandro@gmail.com>
2021-07-12 17:27:07 -0300

    Starting project with git

---
+++ app/src/main/java/com/leandro/instagram/main/presentation/MainActivity.java
@@ -0,0 +1,16 @@
+package com.leandro.instagram.main.presentation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.leandro.instagram.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
