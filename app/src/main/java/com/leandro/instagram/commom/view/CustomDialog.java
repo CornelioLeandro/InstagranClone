1cc0f76c8129ca724743089ebffd57600c77d6e0
InstagramClone <cornelio.leandro@gmail.com>
2021-07-12 10:54:57 -0300

    Starting project with git

---
+++ app/src/main/java/com/leandro/instagram/commom/view/CustomDialog.java
@@ -0,0 +1,106 @@
+package com.leandro.instagram.commom.view;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.ContextThemeWrapper;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.core.content.ContextCompat;
+
+import com.leandro.instagram.R;
+
+public class CustomDialog extends Dialog {
+
+    private LinearLayout dialogLinearlayout;
+    private LinearLayout.LayoutParams layoutParams;
+
+    private TextView titleView;
+    private TextView[] textViews;
+    private int titleId;
+
+    public CustomDialog(@NonNull Context context) {
+        super(context);
+    }
+
+    public CustomDialog(@NonNull Context context, int themeResId) {
+        super(context, themeResId);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dialog_custom);
+
+        layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        dialogLinearlayout = findViewById(R.id.dialog_container);
+        layoutParams.setMargins(30, 50, 30, 50);
+    }
+
+    @Override
+    public void setTitle(int titleId) {
+        this.titleId = titleId;
+    }
+
+    @Override
+    public void show() {
+        super.show();
+        titleView.setText(titleId);
+
+        for (TextView textView : textViews) {
+            dialogLinearlayout.addView(textView, layoutParams);
+        }
+    }
+
+    private void addButton(final View.OnClickListener listener, @StringRes int... texts) {
+        textViews = new TextView[texts.length];
+        for (int i = 0 ; i< texts.length; i++){
+            TextView textView = new TextView(new ContextThemeWrapper(getContext(),R.style.instaTexViewbBaseDialog), null);
+            textView.setId(texts[i]);
+            textView.setText(texts[i]);
+            textView.setOnClickListener(v -> {
+                listener.onClick(v);
+                dismiss();
+            });
+            textViews[i] = textView;
+        }
+    }
+
+    public static class Builder {
+
+        private final Context context;
+        private int titleId;
+        private View.OnClickListener listener;
+        private int[] texts;
+
+        public Builder(Context context) {
+            this.context = context;
+        }
+
+        public Builder setTitle(@StringRes int titleId) {
+            this.titleId = titleId;
+            return this;
+        }
+
+        public Builder addButton(View.OnClickListener listener, @StringRes int... texts) {
+            this.listener = listener;
+            this.texts = texts;
+            return this;
+        }
+
+        public CustomDialog build() {
+            CustomDialog customDialog = new CustomDialog(context);
+            customDialog.setTitle(titleId);
+            customDialog.addButton(listener, texts);
+            return customDialog;
+        }
+    }
+}
