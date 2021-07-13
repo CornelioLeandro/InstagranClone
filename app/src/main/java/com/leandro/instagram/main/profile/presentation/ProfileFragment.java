5274d68e76346da552684165862e91fa4d4cc36e
InstagramClone <cornelio.leandro@gmail.com>
2021-07-12 21:36:04 -0300

    Starting project with git

---
+++ app/src/main/java/com/leandro/instagram/main/profile/presentation/ProfileFragment.java
@@ -0,0 +1,23 @@
+package com.leandro.instagram.main.profile.presentation;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.leandro.instagram.R;
+
+public class ProfileFragment extends Fragment {
+
+    public ProfileFragment(){}
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main_profile, container,false);
+    }
+}
