25d252b4ba8ad5bc7c6c007312704d1d0d691aac
InstagramClone <cornelio.leandro@gmail.com>
2021-11-23 18:45:13 -0300

    ADDCamera e Camera Surface inicio

---
+++ app/src/main/java/com/leandro/instagram/main/camera/presentation/AddActivity.java
@@ -0,0 +1,90 @@
+package com.leandro.instagram.main.camera.presentation;
+
+import android.content.Intent;
+import android.graphics.Camera;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+import com.leandro.instagram.R;
+import com.leandro.instagram.commom.view.AbstractActivity;
+import com.leandro.instagram.main.presentation.Context;
+
+import butterknife.BindView;
+
+public class AddActivity extends AbstractActivity {
+    @BindView(R.id.add_viewpager)
+    ViewPager viewPager;
+
+    @BindView(R.id.add_tab_layout)
+    TabLayout tabLayout;
+
+    public static void launch(Context context) {
+        Intent intent = new Intent(context, AddActivity.class);
+        context.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        if (getSupportActionBar() != null) {
+            Drawable ic_close = findDrawable(R.drawable.ic_close);
+            getSupportActionBar().setHomeAsUpIndicator(ic_close);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setStatusBarColor(findColor(R.color.blueEnable));
+        }
+    }
+
+    @Override
+    protected void onInject() {
+    ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+    viewPager.setAdapter(adapter);
+
+        GalleryFragment galleryFragment = new GalleryFragment();
+        adapter.add(galleryFragment);
+
+        CameraFragment cameraFragment = new CameraFragment();
+        adapter.add(cameraFragment);
+
+        adapter.notifyDataSetChanged();
+        TabLayout.Tab tabLeft = tabLayout.getTabAt(0);
+        if (tabLeft != null)
+            tabLeft.setText(getString(R.string.gallery));
+
+        TabLayout.Tab tabRight = tabLayout.getTabAt(1);
+        if (tabRight != null)
+            tabLeft.setText(getString(R.string.photo));
+
+        viewPager.setCurrentItem(adapter.getCount() -1);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected int getLayout() {
+        return R.layout.activity_add;
+    }
+}
---
+++ app/src/main/java/com/leandro/instagram/main/camera/presentation/GalleryFragment.java
@@ -0,0 +1,11 @@
+package com.leandro.instagram.main.camera.presentation;
+
+import com.leandro.instagram.R;
+import com.leandro.instagram.commom.view.AbstractFragment;
+
+public class GalleryFragment extends AbstractFragment {
+    @Override
+    protected int getLayout() {
+        return R.layout.fragment_main_gallery;
+    }
+}
